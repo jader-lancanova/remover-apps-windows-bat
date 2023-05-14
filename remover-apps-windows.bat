c41ecde9cfd238bf3783e8b870423a47a9ec9eb9
Lançanova <jadder.sil@gmail.com>
2023-05-14 00:36:02 -0300

    Commit  inicial

---
+++ remover-apps-windows.bat
@@ -0,0 +1,56 @@
+# Remover App: 3DBuilder
+powershell.exe -Command "&{Get-AppxPackage *3dbuilder* | Remove-AppxPackage}"
+
+# Remover App: Tenha o Office
+powershell.exe -Command "&{Get-AppxPackage *officehub* | Remove-AppxPackage}"
+
+# Remover App: Skype
+powershell.exe -Command "&{Get-AppxPackage *skypeapp* | Remove-AppxPackage}"
+
+# Remover App: Mapas
+powershell.exe -Command "&{Get-AppxPackage *windowsmaps* | Remove-AppxPackage}"
+
+# Remover App: Solitaire Collection
+powershell.exe -Command "&{Get-AppxPackage *solitairecollection* | Remove-AppxPackage}"
+
+# Remover App: MSN Dinheiro
+powershell.exe -Command "&{Get-AppxPackage *bingfinance* | Remove-AppxPackage}"
+
+# Remover App: Notícias
+powershell.exe -Command "&{Get-AppxPackage *bingnews* | Remove-AppxPackage}"
+
+# Remover App: Pessoas
+powershell.exe -Command "&{Get-AppxPackage *people* | Remove-AppxPackage}"
+
+# Remover App: Phone Companion
+powershell.exe -Command "&{Get-AppxPackage *windowsphone* | Remove-AppxPackage}"
+
+# Remover App: Esportes
+powershell.exe -Command "&{Get-AppxPackage *bingsports* | Remove-AppxPackage}"
+
+# Remover App: Clima
+powershell.exe -Command "&{Get-AppxPackage *bingweather* | Remove-AppxPackage}"
+
+# Remover App: Xbox
+powershell.exe -Command "&{Get-AppxPackage *xboxapp* | Remove-AppxPackage}"
+
+# Remover App: XboxIdentityProvider
+powershell.exe -Command "&{Get-AppxPackage *XboxIdentityProvider* | Remove-AppxPackage}"
+
+# Remover App: XboxGameOverlay
+powershell.exe -Command "&{Get-AppxPackage *XboxGameOverlay* | Remove-AppxPackage}"
+
+# Remover App: Xbox.TCUI
+powershell.exe -Command "&{Get-AppxPackage *Xbox.TCUI* | Remove-AppxPackage}"
+
+# Remover App: XboxSpeechToTextOverlay
+powershell.exe -Command "&{Get-AppxPackage *XboxSpeechToTextOverlay* | Remove-AppxPackage}"
+
+# Remover App: XboxGameCallableUI
+powershell.exe -Command "&{Get-AppxPackage *XboxGameCallableUI* | Remove-AppxPackage}"
+
+# Remover App: Mail
+powershell.exe -Command "&{Get-AppxPackage *mail* | Remove-AppxPackage}"
+
+# Remover App: Print3D
+powershell.exe -Command "&{Get-AppxPackage *Print3D* | Remove-AppxPackage}"
